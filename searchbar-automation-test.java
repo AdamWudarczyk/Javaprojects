dcb3345ca6fcb5615c4eb57e08ad89285f3d7917
AdamWudarczyk <70765358+AdamWudarczyk@users.noreply.github.com>
2020-11-30 21:36:01 +0100

    Create searchbar-automation-test.java

---
+++ searchbar-automation-test.java
@@ -0,0 +1,30 @@
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+
+public class Zadanie8 {
+
+    static WebDriver driver;
+
+    String urlRebel = "https://www.rebel.pl/";
+    String searchedText = "/html/body/header/div[2]/div/nav/form/div[1]/input";
+    String nazwaTagLink = "a";
+    String nazwaTagImage = "img";
+
+    @BeforeAll
+    public static void setUp() {
+        System.setProperty("webdriver.chrome.driver", "C:/Users/Adam/Documents/SELENIUM/chromedriver.exe");
+        driver = new ChromeDriver();
+    }
+
+    @Test
+    public void iloscLinkowNaStronie() {
+        driver.navigate().to(urlRebel);
+        driver.findElement(By.xpath(searchedText)).sendKeys("Nemesis");
+        driver.findElement(By.xpath(searchedText)).sendKeys(Keys.ENTER);
+    }
+}

99890ae5b98107056488ccecc43396b81270c5b9
AdamWudarczyk <70765358+AdamWudarczyk@users.noreply.github.com>
2023-02-07 16:23:00 +0100

    Create SeleniumWD-Tests.java

---
+++ Basics/SeleniumWD-Tests.java
@@ -0,0 +1,46 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+public class WebDriverTest {
+    private WebDriver driver;
+
+    @BeforeTest
+    public void setup() {
+        System.setProperty("webdriver.chrome.driver", "C:/Users/Adam/Downloads/chromedriver_win32/chromedriver.exe");
+        driver = new ChromeDriver();
+    }
+
+    @Test
+    public void testSearch() {
+        driver.get("https://www.google.com");
+        driver.findElement(By.xpath("//input[@name='q']")).sendKeys("Selenium WebDriver");
+        driver.findElement(By.xpath("//input[@name='btnK']")).click();
+        String title = driver.getTitle();
+        assert title.contains("Selenium WebDriver");
+    }
+
+    @Test
+    public void testClickLink() {
+        driver.get("https://www.google.com");
+        driver.findElement(By.xpath("//a[text()='Gmail']")).click();
+        String title = driver.getTitle();
+        assert title.contains("Gmail");
+    }
+
+    @Test
+    public void testEnterText() {
+        driver.get("https://www.google.com");
+        driver.findElement(By.xpath("//input[@name='q']")).sendKeys("Selenium WebDriver");
+        String enteredText = driver.findElement(By.xpath("//input[@name='q']")).getAttribute("value");
+        assert enteredText.equals("Selenium WebDriver");
+    }
+
+    @AfterTest
+    public void teardown() {
+        driver.quit();
+    }
+}
